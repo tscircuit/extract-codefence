004045254b0dbe547bbc3478105a1e0581b7c6fa
seveibar <seveibar@gmail.com>
2024-11-03 17:48:33 -0800

    repro extract code fence doesn't extract when no code type is specified

--- test/extract-codefence.test.ts
+++ test/extract-codefence.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "bun:test"
 import { extractCodefence } from "../index"
 
-test("extractCodefence", () => {
+test("extractCodefence 1", () => {
   expect(extractCodefence("```tsx\nconsole.log('hello')\n```")).toBe("console.log('hello')")
+})
+
+test("extractCodefence 2", () => {
+  expect(extractCodefence("```\nconsole.log('hello')\n```")).toBe("console.log('hello')")
 })
